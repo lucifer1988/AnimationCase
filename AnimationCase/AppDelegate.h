844e42dce967d96726284e4978524830639d471c
alientest <alienetest123@163.com>
2015-11-11 16:19:25 +0800

    initial commit

---
+++ AnimationCase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationCase
+//
+//  Created by liuyi on 15/11/11.
+//  Copyright © 2015年 Lucifer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationCase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationCase
+//
+//  Created by liuyi on 15/11/11.
+//  Copyright © 2015年 Lucifer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
